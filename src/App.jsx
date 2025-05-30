6f7373d4d87ab74d39306afc39e425e6634e4c77
masanori0117 <masanori.takahashi.0117@gmail.com>
2025-05-30 17:12:51 +0900

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">Tailwind 動作確認</h1>
+    </div>
+  );
+}
+
+export default App;
