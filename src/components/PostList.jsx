959a46e7de8723d0a0203c332708b82515784d98
masanori0117 <masanori.takahashi.0117@gmail.com>
2025-06-03 12:49:35 +0900

    課題3の実装

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,22 @@
-
-import { posts } from '../data/posts';
+// import { posts } from '../data/posts';
 import PostCard from './PostCard';
+import { useState, useEffect } from 'react';
 
 export default function PostList() {
+  const [posts, setPosts] = useState([]);
+    useEffect(() => {
+      const fetchPost = async() => {
+        try {
+          const response = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+          const data = await response.json();
+          const postArray = data.posts;
+          setPosts(postArray);
+        } catch(error) {
+          console.error("データ取得失敗:", error);
+        }
+      }
+      fetchPost();
+    }, []);
   return (
     <>
       {posts.map(post => (
